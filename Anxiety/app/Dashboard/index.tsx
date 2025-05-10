0397d25273af4ab9c3f773e3baed350ff94be9df
Caique Kenji <caiquekenjiyafuco@gmail.com>
2025-05-10 19:06:03 -0300

    Home page

--- Anxiety/app/Dashboard/index.tsx
+++ Anxiety/app/Dashboard/index.tsx
@@ -1,3 +1,39 @@
 import { useRouter } from "expo-router";
-
+import React from "react";
+import { View, StyleSheet, SafeAreaView, TouchableOpacity, Image } from "react-native";
 import { styles } from './styles'
+import Svg, { Path, Ellipse } from "react-native-svg";
+import { Ionicons, FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function App() {
+    const router = useRouter();
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topBar}>
+        <TouchableOpacity onPress={() => router.push('/User')}>
+          <Ionicons name="person-circle" size={45} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.caminho}>
+        <Svg height="100%" width="100%">
+        </Svg>
+      </View>
+
+      <View style={styles.botoes}>
+        <TouchableOpacity onPress={() => router.push('/Dashboard')}>
+          <FontAwesome5 name="map" size={25} color="#7B339C" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push('/Calendario')}>
+          <FontAwesome5 name="calendar-alt" size={25} color="#7B339C" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push('/Modo_foco')}>
+          <MaterialCommunityIcons name="school-outline" size={25} color="#7B339C" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push('/Respiracao')}>
+          <MaterialCommunityIcons name="clock" size={25} color="#7B339C" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
--- Anxiety/app/Dashboard/styles.ts
+++ Anxiety/app/Dashboard/styles.ts
@@ -1,7 +1,24 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#7B339C",
+  },
+  topBar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 16,
+  },
+  caminho: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  botoes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "white",
+    paddingVertical: 12,
+  },
+});
